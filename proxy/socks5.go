c8eed5d3f674e174bd12b4ed528554ffab10de58
syntaxErrorLineNULL <u5495527112@gmail.com>
2026-02-06 17:23:22 +0300

    socks5 proxy implementation

--- proxy/socks5.go
+++ proxy/socks5.go
@@ -1,1 +1,44 @@
 package proxy
+
+import (
+	"net/url"
+
+	"github.com/valyala/fasthttp"
+	"github.com/valyala/fasthttp/fasthttpproxy"
+)
+
+type SOCKS5Proxy struct {
+	url, username, password string
+}
+
+func NewSOCKS5Proxy(proxyURL string) (*SOCKS5Proxy, error) {
+	usr, err := url.Parse(proxyURL)
+	if err != nil {
+		return nil, err
+	}
+
+	proxy := &SOCKS5Proxy{url: proxyURL}
+
+	if usr.User != nil {
+		proxy.username = usr.User.Username()
+		proxy.password, _ = usr.User.Password()
+	}
+
+	return proxy, err
+}
+
+func NewSOCKS5ProxyWithAuth(proxyURL, username, password string) (*SOCKS5Proxy, error) {
+	proxy, err := NewSOCKS5Proxy(proxyURL)
+	if err != nil {
+		return nil, err
+	}
+
+	proxy.username = username
+	proxy.password = password
+
+	return proxy, nil
+}
+
+func (s *SOCKS5Proxy) Dial() fasthttp.DialFunc {
+	return fasthttpproxy.FasthttpSocksDialer(s.url)
+}
