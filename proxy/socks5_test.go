ae9448fb65aec512b9031c5c6a59401dcfc8d4ea
syntaxErrorLineNULL <u5495527112@gmail.com>
2026-02-06 18:38:48 +0300

    TestSOCKS5ProxyDialWithSocksServer

--- proxy/socks5_test.go
+++ proxy/socks5_test.go
@@ -1,10 +1,15 @@
 package proxy
 
 import (
+	"fmt"
+	"net"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/things-go/go-socks5"
 	"github.com/valyala/fasthttp"
 )
 
@@ -85,7 +90,7 @@ func TestNewSOCKS5ProxyWithAuth(t *testing.T) {
 	})
 }
 
-func TestNewSOCKS5ProxyDial(t *testing.T) {
+func TestSOCKS5ProxyDial(t *testing.T) {
 	t.Parallel()
 
 	t.Run("SuccessInitDialFunc", func(t *testing.T) {
@@ -124,4 +129,42 @@ func TestNewSOCKS5ProxyDial(t *testing.T) {
 		err = client.Do(req, res)
 		assert.Error(t, err)
 	})
+}
+
+func TestSOCKS5ProxyDialWithSocksServer(t *testing.T) {
+	socksServer := socks5.NewServer()
+
+	listener, err := net.Listen("tcp", "127.0.0.1:0")
+	assert.NoError(t, err)
+
+	defer listener.Close()
+
+	go socksServer.Serve(listener)
+
+	time.Sleep(100 * time.Millisecond)
+
+	targetServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte("success"))
+	}))
+	defer targetServer.Close()
+
+	proxy, err := NewSOCKS5Proxy(fmt.Sprintf("socks5://%s", listener.Addr().String()))
+	assert.NoError(t, err)
+
+	client := &fasthttp.Client{ReadTimeout: 300 * time.Millisecond, WriteTimeout: 300 * time.Millisecond, Dial: proxy.Dial()}
+	assert.NotNil(t, proxy.Dial)
+
+	req := fasthttp.AcquireRequest()
+	res := fasthttp.AcquireResponse()
+	defer fasthttp.ReleaseRequest(req)
+	defer fasthttp.ReleaseResponse(res)
+
+	req.SetRequestURI(targetServer.URL)
+
+	err = client.Do(req, res)
+
+	assert.NoError(t, err)
+	assert.Equal(t, fasthttp.StatusOK, res.StatusCode())
+	assert.Equal(t, "success", string(res.Body()))
 }
