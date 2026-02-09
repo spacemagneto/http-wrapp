c063763c4688660273819954caafc37208693d1f
syntaxErrorLineNULL <u5495527112@gmail.com>
2026-02-09 23:48:43 +0300

    http proxy pkg

--- proxy/http.go
+++ proxy/http.go
@@ -1,1 +1,39 @@
 package proxy
+
+import (
+	"fmt"
+	"net/url"
+	"strings"
+	"time"
+
+	"github.com/valyala/fasthttp"
+	"github.com/valyala/fasthttp/fasthttpproxy"
+)
+
+type HTTPProxy struct {
+	url     string
+	timeout time.Duration
+}
+
+func NewHTTPProxy(proxyURL string, timeout time.Duration) (*HTTPProxy, error) {
+	parseURL, err := url.Parse(proxyURL)
+	if err != nil {
+		return nil, err
+	}
+
+	if !strings.HasPrefix(parseURL.Scheme, "http") {
+		return nil, fmt.Errorf("invalid proxy scheme: expected 'http://' or 'https://', got '%s://'", parseURL.Scheme)
+	}
+
+	proxy := &HTTPProxy{url: proxyURL, timeout: timeout}
+
+	return proxy, err
+}
+
+func (h *HTTPProxy) Dial() fasthttp.DialFunc {
+	if h.timeout > 0 {
+		return fasthttpproxy.FasthttpHTTPDialerDualStackTimeout(h.url, h.timeout)
+	}
+
+	return fasthttpproxy.FasthttpHTTPDialer(h.url)
+}
