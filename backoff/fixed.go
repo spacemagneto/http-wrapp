b07117cecb967e804459881dbde8ced568eb4594
syntaxErrorLineNULL <u5495527112@gmail.com>
2026-02-11 16:48:19 +0300

    fixed strategy

--- backoff/fixed.go
+++ backoff/fixed.go
@@ -1,1 +1,21 @@
 package backoff
+
+import (
+	"time"
+)
+
+type Fixed struct {
+	delay time.Duration
+}
+
+func NewFixed(delay time.Duration) *Fixed {
+	if delay <= 0 {
+		delay = 1 * time.Second
+	}
+
+	return &Fixed{delay: delay}
+}
+
+func (f *Fixed) Next(_ int) time.Duration {
+	return f.delay
+}
