ed5a25fd7c9541f34c67ceded6a1dbcae3cc04b1
syntaxErrorLineNULL <u5495527112@gmail.com>
2026-02-11 16:48:30 +0300

    InitStrategyWithoutDelay

--- backoff/fixed_test.go
+++ backoff/fixed_test.go
@@ -1,7 +1,18 @@
 package backoff
 
-import "testing"
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestFixedBackoffStrategy(t *testing.T) {
 	t.Parallel()
+
+	t.Run("InitStrategyWithoutDelay", func(t *testing.T) {
+		fixed := NewFixed(0)
+		assert.NotNil(t, fixed)
+		assert.Equal(t, 1*time.Second, fixed.delay)
+	})
 }
