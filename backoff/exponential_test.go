77c197697a2483f15fb88f7bc3c49e89df4f2e74
syntaxErrorLineNULL <u5495527112@gmail.com>
2026-02-12 23:19:56 +0300

    InitStrategyWithDefaultStep test case

--- backoff/exponential_test.go
+++ backoff/exponential_test.go
@@ -1,7 +1,24 @@
 package backoff
 
-import "testing"
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestExponentialStrategy(t *testing.T) {
 	t.Parallel()
+
+	t.Run("InitStrategyWithDefaultStep", func(t *testing.T) {
+		baseDelay := 1 * time.Second
+		maxDelay := 10 * time.Second
+
+		exponential := NewExponential(baseDelay, maxDelay)
+		assert.NotNil(t, exponential)
+
+		assert.Equal(t, baseDelay, exponential.baseDelay)
+		assert.Equal(t, maxDelay, exponential.maxDelay)
+		assert.Equal(t, float64(2), exponential.step)
+	})
 }
