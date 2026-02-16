9d15afef4ad17cc81c9c21c3097debcd42815850
syntaxErrorLineNULL <u5495527112@gmail.com>
2026-02-16 20:26:14 +0300

    SuccessInitStrategy test case

--- backoff/jitter_test.go
+++ backoff/jitter_test.go
@@ -1,7 +1,23 @@
 package backoff
 
-import "testing"
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestJitterStrategy(t *testing.T) {
 	t.Parallel()
+
+	t.Run("SuccessInitStrategy", func(t *testing.T) {
+		baseDelay := 1 * time.Second
+		maxDelay := 10 * time.Second
+
+		exponential := NewJitter(baseDelay, maxDelay)
+		assert.NotNil(t, exponential)
+
+		assert.Equal(t, baseDelay, exponential.baseDelay)
+		assert.Equal(t, maxDelay, exponential.maxDelay)
+	})
 }
