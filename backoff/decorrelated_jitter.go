e75236f7642499bdb75c33fb9b2e7107455d3793
syntaxErrorLineNULL <nagornyj.a@bknd.wtf>
2026-02-18 23:43:56 +0300

    implement decorrelated jitter

--- backoff/decorrelated_jitter.go
+++ backoff/decorrelated_jitter.go
@@ -1,1 +1,44 @@
 package backoff
+
+import (
+	"math/rand"
+	"time"
+)
+
+type DecorrelatedJitter struct {
+	baseDelay, maxDelay time.Duration
+}
+
+func NewDecorrelatedJitter(delay, max time.Duration) *DecorrelatedJitter {
+	if delay <= 0 {
+		delay = DefaultJitterBaseDelay
+	}
+
+	if max <= 0 {
+		max = DefaultJitterMaxDelay
+	}
+
+	if max < delay {
+		max = delay
+	}
+
+	return &DecorrelatedJitter{baseDelay: delay, maxDelay: max}
+}
+
+func (d *DecorrelatedJitter) Next(previousDelay int64) time.Duration {
+	// sleep = min(cap, random_between(base, sleep * 3))
+	high := time.Duration(previousDelay) * 3
+	if high < d.baseDelay {
+		high = d.baseDelay
+	}
+	if high > d.maxDelay {
+		high = d.maxDelay
+	}
+
+	diff := int64(high - d.baseDelay)
+	if diff <= 0 {
+		return d.baseDelay
+	}
+
+	return d.baseDelay + time.Duration(rand.Int63n(diff+1))
+}
