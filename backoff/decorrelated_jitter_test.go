46dddb44d76b5995ca8c7cdfb21516a2a3ebb80a
syntaxErrorLineNULL <nagornyj.a@bknd.wtf>
2026-02-19 00:00:45 +0300

    ReturnsNextInStrictRange test case

--- backoff/decorrelated_jitter_test.go
+++ backoff/decorrelated_jitter_test.go
@@ -14,43 +14,44 @@ func TestDecorrelatedJitter(t *testing.T) {
 		baseDelay := 1 * time.Second
 		maxDelay := 10 * time.Second
 
-		exponential := NewDecorrelatedJitter(baseDelay, maxDelay)
-		assert.NotNil(t, exponential)
+		decorrelatedJitter := NewDecorrelatedJitter(baseDelay, maxDelay)
+		assert.NotNil(t, decorrelatedJitter)
 
-		assert.Equal(t, baseDelay, exponential.baseDelay)
-		assert.Equal(t, maxDelay, exponential.maxDelay)
+		assert.Equal(t, baseDelay, decorrelatedJitter.baseDelay)
+		assert.Equal(t, maxDelay, decorrelatedJitter.maxDelay)
 	})
 
 	t.Run("InitStrategyWithDefaultValDelay", func(t *testing.T) {
-		exponential := NewDecorrelatedJitter(0, 0)
-		assert.NotNil(t, exponential)
+		decorrelatedJitter := NewDecorrelatedJitter(0, 0)
+		assert.NotNil(t, decorrelatedJitter)
 
-		assert.Equal(t, DefaultJitterBaseDelay, exponential.baseDelay)
-		assert.Equal(t, DefaultJitterMaxDelay, exponential.maxDelay)
+		assert.Equal(t, DefaultJitterBaseDelay, decorrelatedJitter.baseDelay)
+		assert.Equal(t, DefaultJitterMaxDelay, decorrelatedJitter.maxDelay)
 	})
 
 	t.Run("InitStrategyWithInvalidArgs", func(t *testing.T) {
-		exponential := NewDecorrelatedJitter(0, -10*time.Second)
-		assert.NotNil(t, exponential)
+		decorrelatedJitter := NewDecorrelatedJitter(0, -10*time.Second)
+		assert.NotNil(t, decorrelatedJitter)
 
-		assert.Equal(t, DefaultJitterBaseDelay, exponential.baseDelay)
-		assert.Equal(t, DefaultJitterMaxDelay, exponential.maxDelay)
+		assert.Equal(t, DefaultJitterBaseDelay, decorrelatedJitter.baseDelay)
+		assert.Equal(t, DefaultJitterMaxDelay, decorrelatedJitter.maxDelay)
 	})
 
 	t.Run("InitStrategyWhenMaxLessThanBase", func(t *testing.T) {
 		baseDelay := 5 * time.Second
 		maxDelay := 1 * time.Second
-		jitter := NewDecorrelatedJitter(baseDelay, maxDelay)
 
-		assert.Equal(t, baseDelay, jitter.maxDelay)
+		decorrelatedJitter := NewDecorrelatedJitter(baseDelay, maxDelay)
+
+		assert.Equal(t, baseDelay, decorrelatedJitter.maxDelay)
 	})
 
 	t.Run("SuccessGetNext", func(t *testing.T) {
 		baseDelay := 1 * time.Second
 		maxDelay := 10 * time.Second
 
-		equalJitter := NewDecorrelatedJitter(baseDelay, maxDelay)
-		delay := equalJitter.Next(1)
+		decorrelatedJitter := NewDecorrelatedJitter(baseDelay, maxDelay)
+		delay := decorrelatedJitter.Next(1)
 		assert.True(t, delay >= 0)
 	})
 
@@ -58,7 +59,26 @@ func TestDecorrelatedJitter(t *testing.T) {
 		baseDelay := 1 * time.Second
 		maxDelay := 10 * time.Second
 
-		dj := NewDecorrelatedJitter(baseDelay, maxDelay)
-		assert.Equal(t, baseDelay, dj.Next(0))
+		decorrelatedJitter := NewDecorrelatedJitter(baseDelay, maxDelay)
+		assert.Equal(t, baseDelay, decorrelatedJitter.Next(0))
+	})
+
+	t.Run("ReturnsNextInStrictRange", func(t *testing.T) {
+		baseDelay := 1 * time.Second
+		maxDelay := 10 * time.Second
+
+		decorrelatedJitter := NewEqualJitter(baseDelay, maxDelay)
+
+		// If the previous delay was 2 seconds:
+		// high = min(10s, 2s * 3) = 6s
+		// The range should be [baseDelay, high], i.e. [1s, 6s]
+		minExpected := 1 * time.Second
+		maxExpected := 6 * time.Second
+
+		for i := 0; i < 15; i++ {
+			delay := decorrelatedJitter.Next(2)
+			assert.True(t, delay >= minExpected, "Delay %v should be >= %v", delay, minExpected)
+			assert.True(t, delay < maxExpected, "Delay %v should be < %v", delay, maxExpected)
+		}
 	})
 }
