0a943762e5a72a020e5b1c6c6d6ae291c05e70db
syntaxErrorLineNULL <nagornyj.a@bknd.wtf>
2026-02-18 23:44:15 +0300

    base test cases

--- backoff/decorrelated_jitter_test.go
+++ backoff/decorrelated_jitter_test.go
@@ -1,1 +1,47 @@
 package backoff
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDecorrelatedJitter(t *testing.T) {
+	t.Parallel()
+
+	t.Run("SuccessInitStrategy", func(t *testing.T) {
+		baseDelay := 1 * time.Second
+		maxDelay := 10 * time.Second
+
+		exponential := NewDecorrelatedJitter(baseDelay, maxDelay)
+		assert.NotNil(t, exponential)
+
+		assert.Equal(t, baseDelay, exponential.baseDelay)
+		assert.Equal(t, maxDelay, exponential.maxDelay)
+	})
+
+	t.Run("InitStrategyWithDefaultValDelay", func(t *testing.T) {
+		exponential := NewDecorrelatedJitter(0, 0)
+		assert.NotNil(t, exponential)
+
+		assert.Equal(t, DefaultJitterBaseDelay, exponential.baseDelay)
+		assert.Equal(t, DefaultJitterMaxDelay, exponential.maxDelay)
+	})
+
+	t.Run("InitStrategyWithInvalidArgs", func(t *testing.T) {
+		exponential := NewDecorrelatedJitter(0, -10*time.Second)
+		assert.NotNil(t, exponential)
+
+		assert.Equal(t, DefaultJitterBaseDelay, exponential.baseDelay)
+		assert.Equal(t, DefaultJitterMaxDelay, exponential.maxDelay)
+	})
+
+	t.Run("InitStrategyWhenMaxLessThanBase", func(t *testing.T) {
+		baseDelay := 5 * time.Second
+		maxDelay := 1 * time.Second
+		jitter := NewDecorrelatedJitter(baseDelay, maxDelay)
+
+		assert.Equal(t, baseDelay, jitter.maxDelay)
+	})
+}
