d2dfb8774dbfe7910cf8a8cafde154c3ca447082
syntaxErrorLineNULL <u5495527112@gmail.com>
2026-02-25 19:06:09 +0300

    clear

--- backoff/exponential_jitter_test.go
+++
@@ -1,8 +0,0 @@
-package backoff
-
-import "testing"
-
-func TestExponentialJitter(t *testing.T) {
-	// temp = min(cap, base * 2 ** attempt)
-	// sleep = temp / 2 + random_between(0, temp / 2)
-}
--- transport.go
+++
@@ -1,7 +0,0 @@
-package client
-
-import "github.com/valyala/fasthttp"
-
-type Transport interface {
-	Do(req *fasthttp.Request, res *fasthttp.Response) error
-}
