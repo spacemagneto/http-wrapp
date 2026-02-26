3d9d9a8d9c3ee18908c078e93abf587807cf173e
syntaxErrorLineNULL <u5495527112@gmail.com>
2026-02-26 23:13:14 +0300

    round robin rotation implement

--- roundrobin.go
+++ roundrobin.go
@@ -1,1 +1,16 @@
 package client
+
+import "sync/atomic"
+
+type RoundRobin struct {
+	counter atomic.Uint64
+}
+
+func (r *RoundRobin) Next(entries []Proxy) Proxy {
+	if len(entries) == 0 {
+		return nil
+	}
+
+	index := r.counter.Add(1) % uint64(len(entries))
+	return entries[index]
+}
