ef7d05c02e5444d0f81987ece4f224af06fee7b6
syntaxErrorLineNULL <u5495527112@gmail.com>
2026-02-26 23:46:59 +0300

    get proxy method

--- entry.go
+++ entry.go
@@ -4,3 +4,11 @@ type entry struct {
 	proxy Proxy
 	stats proxyStats
 }
+
+func newEntry(proxy Proxy) *entry {
+	return &entry{proxy: proxy}
+}
+
+func (e *entry) Proxy() Proxy {
+	return e.proxy
+}
