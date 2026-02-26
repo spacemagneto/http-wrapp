8d3ba005f8380e7dca7f967eaaab24c378a0b3b6
syntaxErrorLineNULL <u5495527112@gmail.com>
2026-02-26 23:23:00 +0300

    EmptyEntries test case

--- roundrobin_test.go
+++ roundrobin_test.go
@@ -1,7 +1,21 @@
 package client
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestRoundRobin(t *testing.T) {
 	t.Parallel()
+
+	t.Run("EmptyEntries", func(t *testing.T) {
+		roundRobin := &RoundRobin{}
+		assert.NotNil(t, roundRobin)
+		assert.Equal(t, uint64(0), roundRobin.counter.Load())
+
+		nextProxy := roundRobin.Next(nil)
+		assert.Nil(t, nextProxy)
+		assert.Equal(t, uint64(0), roundRobin.counter.Load())
+	})
 }
