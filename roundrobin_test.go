eba5c3182f7e01d0f30eff0b7fca30e826d1dd16
syntaxErrorLineNULL <u5495527112@gmail.com>
2026-02-26 23:27:54 +0300

    SuccessGetNextProxy test case

--- roundrobin_test.go
+++ roundrobin_test.go
@@ -4,7 +4,16 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/valyala/fasthttp"
 )
+
+type mockProxy struct {
+	id int
+}
+
+func (m *mockProxy) Dial() fasthttp.DialFunc {
+	return nil
+}
 
 func TestRoundRobin(t *testing.T) {
 	t.Parallel()
@@ -17,5 +26,17 @@ func TestRoundRobin(t *testing.T) {
 		nextProxy := roundRobin.Next(nil)
 		assert.Nil(t, nextProxy)
 		assert.Equal(t, uint64(0), roundRobin.counter.Load())
+	})
+
+	t.Run("SuccessGetNextProxy", func(t *testing.T) {
+		roundRobin := &RoundRobin{}
+		assert.NotNil(t, roundRobin)
+		assert.Equal(t, uint64(0), roundRobin.counter.Load())
+
+		entries := []Proxy{&mockProxy{id: 1}, &mockProxy{id: 2}, &mockProxy{id: 3}}
+		assert.Equal(t, entries[1], roundRobin.Next(entries))
+		assert.Equal(t, entries[2], roundRobin.Next(entries))
+		assert.Equal(t, entries[0], roundRobin.Next(entries))
+		assert.Equal(t, entries[1], roundRobin.Next(entries))
 	})
 }
