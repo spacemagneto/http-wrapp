0ac3e07148ee808a4ce906b0540bc4af13286406
syntaxErrorLineNULL <u5495527112@gmail.com>
2026-03-02 12:40:50 +0300

    random rotation impl

--- random.go
+++ random.go
@@ -1,1 +1,13 @@
 package client
+
+import "math/rand/v2"
+
+type Random struct{}
+
+func (r *Random) Next(entries []*Entry) *Entry {
+	if len(entries) == 0 {
+		return nil
+	}
+
+	return entries[rand.IntN(len(entries))]
+}
