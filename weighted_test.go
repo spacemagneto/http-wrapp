84bff4b1d41f505c9ca873dd7e69f23a9cf34e46
syntaxErrorLineNULL <u5495527112@gmail.com>
2026-03-02 17:59:37 +0300

    ProxySelectionWithWeightPriorityCalculation test case

--- weighted_test.go
+++ weighted_test.go
@@ -9,11 +9,36 @@ import (
 func TestWeightedRandom(t *testing.T) {
 	t.Parallel()
 
-	t.Run("EmptyEntries", func(t *testing.T) {
-		weighted := &WeightedRandom{}
-		assert.NotNil(t, weighted)
+	weightedRandom := &WeightedRandom{}
+	assert.NotNil(t, weightedRandom)
 
-		nextEntry := weighted.Next(nil)
+	t.Run("EmptyEntries", func(t *testing.T) {
+		nextEntry := weightedRandom.Select(nil)
 		assert.Nil(t, nextEntry)
+	})
+
+	t.Run("ProxySelectionWithWeightPriorityCalculation", func(t *testing.T) {
+		first := &Entry{proxy: &mockProxy{id: 1}}
+		first.stats.RecordSuccess()
+		first.stats.RecordLatency(100)
+
+		second := &Entry{proxy: &mockProxy{id: 2}}
+		second.stats.RecordSuccess()
+		second.stats.RecordLatency(500)
+
+		entries := []*Entry{first, second}
+		counts := make(map[int]int)
+		iterations := 10000
+
+		for i := 0; i < iterations; i++ {
+			selected := weightedRandom.Select(entries)
+			counts[selected.proxy.(*mockProxy).id]++
+		}
+
+		highestPriorityProxy := float64(counts[1]) / float64(iterations)
+		otherPriorityProxy := float64(counts[2]) / float64(iterations)
+
+		assert.InDelta(t, 0.83, highestPriorityProxy, 0.05)
+		assert.InDelta(t, 0.17, otherPriorityProxy, 0.05)
 	})
 }
