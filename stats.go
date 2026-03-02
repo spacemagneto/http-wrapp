4f1729f06aec9ed0b8e67663941f27da556927c5
syntaxErrorLineNULL <u5495527112@gmail.com>
2026-03-02 16:31:14 +0300

    base proxy stats

--- stats.go
+++ stats.go
@@ -1,4 +1,13 @@
 package client
 
-type proxyStats struct {
+import "sync/atomic"
+
+type Stats struct {
+	failsCount atomic.Int64
+
+	successCount atomic.Int64
+}
+
+func NewStats() *Stats {
+	return &Stats{}
 }
