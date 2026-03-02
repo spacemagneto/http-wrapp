3ecffe43debe433d763664fe3ab557550c374f2c
syntaxErrorLineNULL <u5495527112@gmail.com>
2026-03-02 17:39:58 +0300

    WeightedRandom proxy selector impl

--- weighted.go
+++ weighted.go
@@ -1,12 +1,27 @@
 package client
 
-type Weighted struct {
+import "math/rand/v2"
+
+type WeightedRandom struct {
 }
 
-func (Weighted) Next(entries []*Entry) *Entry {
+func (WeightedRandom) Select(entries []*Entry) *Entry {
 	if len(entries) == 0 {
 		return nil
 	}
 
-	return nil
+	var sum float64
+	for _, entry := range entries {
+		sum += entry.stats.Weight()
+	}
+
+	r := rand.Float64() * sum
+	for _, entry := range entries {
+		r -= entry.stats.Weight()
+		if r <= 0 {
+			return entry
+		}
+	}
+
+	return entries[len(entries)-1]
 }
