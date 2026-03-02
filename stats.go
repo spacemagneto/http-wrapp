5fc9841b3af10a20f389d601f73abce89084a9bd
syntaxErrorLineNULL <u5495527112@gmail.com>
2026-03-02 16:43:43 +0300

    RecordFailed

--- stats.go
+++ stats.go
@@ -1,15 +1,22 @@
 package client
 
-import "sync/atomic"
+import (
+	"sync/atomic"
+	"time"
+)
 
 type Stats struct {
 	// consecutiveFails counts failures in a row since the last success.
 	// This is the value checked for quarantine: it resets on every success.
 	consecutiveFails atomic.Int32
 
-	failsCount atomic.Int64
+	failCount atomic.Int64
 
 	successCount atomic.Int64
+
+	// lastFailedUnix stores the UnixNano timestamp of the most recent failure.
+	// Used by Entry.IsHealthy to determine if the cooldown window has elapsed.
+	lastFailedUnix atomic.Int64
 }
 
 func NewStats() *Stats {
@@ -19,4 +26,10 @@ func NewStats() *Stats {
 func (s *Stats) RecordSuccess() {
 	s.successCount.Add(1)
 	s.consecutiveFails.Store(0)
+}
+
+func (s *Stats) RecordFailed() {
+	s.consecutiveFails.Add(1)
+	s.failCount.Add(1)
+	s.lastFailedUnix.Store(time.Now().UnixNano())
 }
