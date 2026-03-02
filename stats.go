f834c5ef3cb2a28c96658b4a38548bbcb4bc8a4f
syntaxErrorLineNULL <u5495527112@gmail.com>
2026-03-02 16:40:41 +0300

    RecordSuccess

--- stats.go
+++ stats.go
@@ -3,6 +3,10 @@ package client
 import "sync/atomic"
 
 type Stats struct {
+	// consecutiveFails counts failures in a row since the last success.
+	// This is the value checked for quarantine: it resets on every success.
+	consecutiveFails atomic.Int32
+
 	failsCount atomic.Int64
 
 	successCount atomic.Int64
@@ -10,4 +14,9 @@ type Stats struct {
 
 func NewStats() *Stats {
 	return &Stats{}
+}
+
+func (s *Stats) RecordSuccess() {
+	s.successCount.Add(1)
+	s.consecutiveFails.Store(0)
 }
