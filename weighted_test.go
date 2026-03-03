3bb9ec1af4b4cdc93bbe6dd0e17d1a564c23ec02
syntaxErrorLineNULL <u5495527112@gmail.com>
2026-03-03 12:19:44 +0300

    SelectLastProxyWithMockRandFunc test case

--- weighted_test.go
+++ weighted_test.go
@@ -9,15 +9,18 @@ import (
 func TestWeightedRandom(t *testing.T) {
 	t.Parallel()
 
-	weightedRandom := &WeightedRandom{}
-	assert.NotNil(t, weightedRandom)
-
 	t.Run("EmptyEntries", func(t *testing.T) {
+		weightedRandom := NewWeightedRandom()
+		assert.NotNil(t, weightedRandom)
+
 		nextEntry := weightedRandom.Select(nil)
 		assert.Nil(t, nextEntry)
 	})
 
 	t.Run("ProxySelectionWithWeightPriorityCalculation", func(t *testing.T) {
+		weightedRandom := NewWeightedRandom()
+		assert.NotNil(t, weightedRandom)
+
 		first := &Entry{proxy: &mockProxy{id: 1}}
 		first.stats.RecordSuccess()
 		first.stats.RecordLatency(100)
@@ -40,5 +43,21 @@ func TestWeightedRandom(t *testing.T) {
 
 		assert.InDelta(t, 0.83, highestPriorityProxy, 0.05)
 		assert.InDelta(t, 0.17, otherPriorityProxy, 0.05)
+	})
+
+	t.Run("SelectLastProxyWithMockRandFunc", func(t *testing.T) {
+		weightedRandom := &WeightedRandom{
+			randFloat64: func() float64 { return 1.1 },
+		}
+
+		assert.NotNil(t, weightedRandom)
+		assert.NotNil(t, weightedRandom.randFloat64)
+
+		entries := []*Entry{{proxy: &mockProxy{id: 1}}, {proxy: &mockProxy{id: 2}}}
+
+		selected := weightedRandom.Select(entries)
+
+		assert.NotNil(t, selected)
+		assert.Equal(t, 2, selected.proxy.(*mockProxy).id, "Should return last entry when r exceeds sum")
 	})
 }
