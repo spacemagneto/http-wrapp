6340695395c99f62b5209714a214f4150922ca17
syntaxErrorLineNULL <u5495527112@gmail.com>
2026-03-04 17:35:47 +0300

    base pool config

--- pool.go
+++ pool.go
@@ -1,5 +1,21 @@
 package client
 
+import "time"
+
+type PoolConfig struct {
+	MaxFails int64
+	Cooldown time.Duration
+	Selector Selector
+}
+
+func defaultPoolConfig() PoolConfig {
+	return PoolConfig{
+		MaxFails: 0,
+		Cooldown: 0,
+		Selector: nil,
+	}
+}
+
 type Pool struct {
 }
 
