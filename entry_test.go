3f46f051de2b5730c7d8037ebc3c3433933c54e4
syntaxErrorLineNULL <u5495527112@gmail.com>
2026-03-04 16:32:18 +0300

    InitialEntry test case

--- entry_test.go
+++ entry_test.go
@@ -1,7 +1,20 @@
 package client
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestProxyEntry(t *testing.T) {
+	t.Parallel()
 
+	t.Run("InitialEntry", func(t *testing.T) {
+		expectProxy := &mockProxy{id: 1}
+		entry := newEntry(expectProxy)
+
+		assert.NotNil(t, entry)
+		assert.Equal(t, expectProxy, entry.Proxy())
+		assert.NotNil(t, entry.Stats())
+	})
 }
