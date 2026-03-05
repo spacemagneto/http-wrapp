8baad2a6560b42da6e68db6e39a9f84088362121
syntaxErrorLineNULL <u5495527112@gmail.com>
2026-03-05 13:34:37 +0300

    pool constructor

--- pool.go
+++ pool.go
@@ -3,22 +3,44 @@ package client
 import "time"
 
 type PoolConfig struct {
-	MaxFails int64
-	Cooldown time.Duration
-	Selector Selector
+	MaxFails       int64
+	CooldownWindow time.Duration
+	Selector       Selector
 }
 
 func defaultPoolConfig() PoolConfig {
 	return PoolConfig{
-		MaxFails: 0,
-		Cooldown: 0,
-		Selector: nil,
+		MaxFails:       0,
+		CooldownWindow: 0,
+		Selector:       nil,
 	}
 }
 
 type Pool struct {
+	entries []*Entry
 }
 
-func NewPool() *Pool {
-	return &Pool{}
+func NewPool(proxies []Proxy, cfg *PoolConfig) *Pool {
+	defaultCfg := defaultPoolConfig()
+
+	if cfg.MaxFails == 0 {
+		cfg.MaxFails = defaultCfg.MaxFails
+	}
+
+	if cfg.CooldownWindow == 0 {
+		cfg.CooldownWindow = defaultCfg.CooldownWindow
+	}
+
+	if cfg.Selector == nil {
+		cfg.Selector = defaultCfg.Selector
+	}
+
+	entries := make([]*Entry, len(proxies))
+	for _, proxy := range proxies {
+		entries = append(entries, newEntry(proxy))
+	}
+
+	return &Pool{
+		entries: entries,
+	}
 }
