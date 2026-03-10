151c0e950929ee4c827ab67c06c718ad323c562e
syntaxErrorLineNULL <u5495527112@gmail.com>
2026-03-10 13:23:21 +0300

    pick proxy method in pool

--- pool.go
+++ pool.go
@@ -1,6 +1,9 @@
 package client
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
 type PoolConfig struct {
 	MaxFails       int64
@@ -10,17 +13,19 @@ type PoolConfig struct {
 
 func defaultPoolConfig() PoolConfig {
 	return PoolConfig{
-		MaxFails:       0,
-		CooldownWindow: 0,
-		Selector:       nil,
+		MaxFails:       3,
+		CooldownWindow: 30 * time.Second,
+		Selector:       &RoundRobinSelector{},
 	}
 }
 
 type Pool struct {
+	mutex   sync.RWMutex
 	entries []*Entry
+	cfg     PoolConfig
 }
 
-func NewPool(proxies []Proxy, cfg *PoolConfig) *Pool {
+func NewPool(proxies []Proxy, cfg PoolConfig) *Pool {
 	defaultCfg := defaultPoolConfig()
 
 	if cfg.MaxFails == 0 {
@@ -40,7 +45,36 @@ func NewPool(proxies []Proxy, cfg *PoolConfig) *Pool {
 		entries = append(entries, newEntry(proxy))
 	}
 
-	return &Pool{
-		entries: entries,
+	return &Pool{entries: entries, cfg: cfg}
+}
+
+func (p *Pool) Pick() (*Entry, error) {
+	p.mutex.RLock()
+	healthy := p.healthyEntries()
+	all := p.entries
+	p.mutex.RUnlock()
+
+	list := healthy
+	if len(list) == 0 {
+		if len(all) == 0 {
+			return nil, ErrNoProxies
+		}
+
+		list = all
 	}
+
+	entry := p.cfg.Selector.Select(list)
+	return entry, nil
+}
+
+func (p *Pool) healthyEntries() []*Entry {
+	healthyProxies := make([]*Entry, 0)
+
+	for _, entry := range p.entries {
+		if entry.HealthCheck(p.cfg.MaxFails, p.cfg.CooldownWindow) {
+			healthyProxies = append(healthyProxies, entry)
+		}
+	}
+
+	return healthyProxies
 }
