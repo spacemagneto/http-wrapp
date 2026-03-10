ae050b0f2dedd5ec8653b93a59b6c8ed0888649e
syntaxErrorLineNULL <u5495527112@gmail.com>
2026-03-10 15:18:52 +0300

    EmptyHealthyEntriesWithAllProxyInQuarantine test case

--- pool_test.go
+++ pool_test.go
@@ -40,4 +40,23 @@ func TestProxyPool(t *testing.T) {
 		assert.Equal(t, pool.entries[0], healthy[0])
 		assert.Equal(t, pool.entries[1], healthy[1])
 	})
+
+	t.Run("EmptyHealthyEntriesWithAllProxyInQuarantine", func(t *testing.T) {
+		proxies := []Proxy{&mockProxy{id: 1}, &mockProxy{id: 2}}
+
+		cfg := PoolConfig{MaxFails: 2, CooldownWindow: time.Minute}
+
+		pool := NewPool(proxies, cfg)
+		assert.NotNil(t, pool)
+
+		pool.entries[0].Stats().RecordFailed()
+		pool.entries[0].Stats().RecordFailed()
+
+		pool.entries[1].Stats().RecordFailed()
+		pool.entries[1].Stats().RecordFailed()
+
+		healthy := pool.healthyEntries()
+
+		assert.Empty(t, healthy)
+	})
 }
