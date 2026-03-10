04f105699e278e73ee14345740028d3de03014ac
syntaxErrorLineNULL <u5495527112@gmail.com>
2026-03-10 13:41:16 +0300

    DefaultPoolConfigValues test case

--- pool_test.go
+++ pool_test.go
@@ -1,7 +1,23 @@
 package client
 
-import "testing"
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestProxyPool(t *testing.T) {
 	t.Parallel()
+
+	t.Run("DefaultPoolConfigValues", func(t *testing.T) {
+		cfg := defaultPoolConfig()
+
+		assert.Equal(t, int64(3), cfg.MaxFails)
+		assert.Equal(t, 30*time.Second, cfg.CooldownWindow)
+		assert.NotNil(t, cfg.Selector)
+
+		_, ok := cfg.Selector.(*RoundRobinSelector)
+		assert.True(t, ok)
+	})
 }
