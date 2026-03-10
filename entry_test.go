d042f43e3175e3adb90d118f445955e7d6bf697d
syntaxErrorLineNULL <u5495527112@gmail.com>
2026-03-10 12:53:39 +0300

    HealthCheckHealthy test case

--- entry_test.go
+++ entry_test.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -16,5 +17,17 @@ func TestProxyEntry(t *testing.T) {
 		assert.NotNil(t, entry)
 		assert.Equal(t, expectProxy, entry.Proxy())
 		assert.NotNil(t, entry.Stats())
+	})
+
+	t.Run("HealthCheckHealthy", func(t *testing.T) {
+		expectProxy := &mockProxy{id: 1}
+		entry := newEntry(expectProxy)
+
+		assert.NotNil(t, entry)
+		assert.Equal(t, expectProxy, entry.Proxy())
+		assert.NotNil(t, entry.Stats())
+
+		isHealthy := entry.HealthCheck(3, time.Minute)
+		assert.True(t, isHealthy)
 	})
 }
